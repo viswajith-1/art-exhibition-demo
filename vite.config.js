41fd2fd3041dd5c00904f99570812b1cc193325c
Viswajith S B <121398392+viswajith-1@users.noreply.github.com>
2025-10-20 18:18:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH
-})
+  base:
+    process.env.NODE_ENV === "production"
+      ? process.env.VITE_BASE_PATH || "/art-exhibition-demo"
+      : "/",
+});
